11eccf096830df0b7d52527e78671f5e468da0ee
Arjun Kumar <93002735+arjunroy7979@users.noreply.github.com>
2023-07-11 21:48:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,21 @@ const hostName = '127.0.0.8'
 
 app.use(express.json())
 app.use(express.urlencoded())
-app.use(cors())
+app.use(cors(
+    {
+  "headers": [
+    {
+      "source": "/api/(.*)",
+      "headers": [
+        { "key": "Access-Control-Allow-Credentials", "value": "true" },
+        { "key": "Access-Control-Allow-Origin", "value": "*" },
+        { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+        { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+      ]
+    }
+  ]
+}
+))
 
 
 app.use("/", userRoutes);
