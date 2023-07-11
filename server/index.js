f55fb7967b6eac1b9f8e481145c9d652bdd93ef8
Arjun Kumar <93002735+arjunroy7979@users.noreply.github.com>
2023-07-11 21:41:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors(
     {
-        origin:["https://mern-crud-client-alpha.vercel.app"],
+        origin:["*"],
         method:["POST","GET"],
         credentials:true
     }
